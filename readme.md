deeb4e381d7ab437c0c71c51020ceb03d2ce2f80
Christian Wasserthal <aperion@fsfe.org>
2021-11-08 23:52:43 +0100

    Add some files

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Plank App
+
+This is it. The App you have always been waiting for. It's here.
+
+Follow [this link](https://cwassert.github.io/plank/index.html) and chose on `Install` in your browser menu.
---
+++ serviceworker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open('plank-cache').then(cache => Promise.all([
+    cache.add('index.html'),
+    cache.add('serviceworker.js'),
+    cache.add('plank.svg'),
+    cache.add('plank.png')
+  ])));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
